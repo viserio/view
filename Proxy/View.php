a799d19afb6897cda9e09c2cff86800148b19397
Daniel Bannert <prisis@users.noreply.github.com>
2017-06-20 23:34:22 +0200

    Fix proxies and use nulllogger (#592)

--- Proxy/View.php
+++ Proxy/View.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-namespace Viserio\Component\View\Proxie;
+namespace Viserio\Component\View\Proxy;
 
 use Interop\Http\Factory\ResponseFactoryInterface;
 use Interop\Http\Factory\StreamFactoryInterface;
